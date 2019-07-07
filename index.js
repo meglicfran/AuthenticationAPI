d68c7a3ab389c348f65900d7e1c080c4d709b88f
meglicfran <meglic.fran@gmail.com>
2019-07-07 10:32:27 +0200

    .

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
 const authRoute = require("./routes/auth");
 
 dotenv.config();
 
+//connnect to db
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log("connected to DB.");
 });
-
+//ttransform request in javascript objects
 app.use(express.json());
 
-//console.log(authRoute);
+//localhost:3000/api/user/*
 app.use("/api/user", authRoute);
 
+// start a server at localhost:3000
 app.listen(3000, () => {
   console.log("Server is up!");
 });
